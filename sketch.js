5f7f865d49290b59f268e04369377195d2cf34bc
Kathryn Spahn <kathrynspahn@Kathryns-MacBook-Pro.local>
2019-08-29 14:19:13 -0500

    my first assignment commit

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 function setup(){
-    createCanvas(400,400);
+    createCanvas(500,500);
     background(255,255,255);
     fill(0,0,0);
 
@@ -22,6 +22,25 @@ function setup(){
     noStroke(),
     ellipse(200,100,50,50)
 
+    //Long
+    noStroke();
+    ellipse(250,100,50,200)
+    //1
+    noStroke(),
+    ellipse (350,50,50,50)
+    //2
+    noStroke(),
+    ellipse(350,100,50,50)
+    //3
+    noStroke(),
+    ellipse(350,150,50,50)
+    //L
+    noStroke(),
+    ellipse(300,100,50,50)
+    //R
+    noStroke(),
+    ellipse(400,100,50,50)
+
     noStroke(),
     ellipse(50,250,50,50)
 
@@ -40,6 +59,26 @@ function setup(){
     //Long
     noStroke(),
     ellipse(200,250,50,200)
+
+
+    noStroke(),
+    ellipse(250,250,50,50)
+
+    noStroke(),
+    ellipse(300,250,50,50)
+
+    noStroke(),
+    ellipse(350,250,50,50)
+
+    noStroke(),
+    ellipse(300,200,50,50)
+
+    noStroke(),
+    ellipse(300,300,50,50)
+
+    //Long
+    noStroke(),
+    ellipse(400,250,50,200)
 
     
 
